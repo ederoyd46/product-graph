9c8fb1864d62dce00272b1ab1dd8b7d90fd9a45e
Matthew Brown <matt@ederoyd.co.uk>
2024-09-05 21:29:08 +0100

    updated nix files

--- flake.nix
+++ flake.nix
@@ -1,44 +1,49 @@
 {
+  description = "Rust project with cross-compilation to MUSL and WASM";
+
   inputs = {
-    nixpkgs.url = "nixpkgs/nixos-unstable";
-
-    fenix = {
-      url = "github:nix-community/fenix";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-
-    flake-utils.url = "github:numtide/flake-utils";
-
-    naersk = {
-      url = "github:nix-community/naersk";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    rust-overlay.url = "github:oxalica/rust-overlay";
   };
 
-  outputs = { self, fenix, flake-utils, naersk, nixpkgs }:
-    flake-utils.lib.eachDefaultSystem (system: {
-      packages.default =
-        let
-          pkgs = nixpkgs.legacyPackages.${system};
-          target = "x86_64-unknown-linux-musl";
-          toolchain = with fenix.packages.${system}; combine [
-            minimal.cargo
-            minimal.rustc
-            targets.${target}.latest.rust-std
-          ];
-        in
+  outputs =
+    {
+      self,
+      nixpkgs,
+      rust-overlay,
+    }:
+    let
+      system = "aarch64-darwin";
+      overlays = [ (import rust-overlay) ];
+      pkgs = import nixpkgs { inherit system overlays; };
 
-        (naersk.lib.${system}.override {
-          cargo = toolchain;
-          rustc = toolchain;
-        }).buildPackage {
-          src = ./.;
-          CARGO_BUILD_TARGET = target;
-          CARGO_TARGET_X86_64_UNKNOWN_LINUX_MUSL_LINKER =
-            let
-              inherit (pkgs.pkgsCross.musl64.stdenv) cc;
-            in
-            "${cc}/bin/${cc.targetPrefix}cc";
-        };
-    });
+      rustSetup = pkgs.rust-bin.stable.latest.default.override {
+        extensions = [
+          "rust-src"
+          "rustfmt"
+          "rust-analyzer"
+          "rust-docs"
+        ];
+        targets = [
+          "x86_64-unknown-linux-musl"
+          "wasm32-wasi"
+        ];
+      };
+
+    in
+    {
+      devShell = pkgs.mkShell {
+        buildInputs =
+          with pkgs;
+          [
+            zig
+            cargo-lambda
+            cargo-zigbuild
+            # darwin.apple_sdk.frameworks.Security
+            darwin.Security
+          ]
+          ++ [ rustSetup ];
+
+      };
+    };
 }
